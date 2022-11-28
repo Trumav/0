a20cd27ddb514ceb01906602b00e7b50ea96de56
linkkalian <105465912+linkkalian@users.noreply.github.com>
2022-11-28 21:05:45 +0700

    Update vmnone.sh

--- vmnone.sh
+++ vmnone.sh
@@ -24,3 +24,28 @@ sed -i '$ iproxy_set_header Upgrade \$http_upgrade;' /etc/nginx/conf.d/vmnone.co
 sed -i '$ iproxy_set_header Connection "upgrade";' /etc/nginx/conf.d/vmnone.conf
 sed -i '$ iproxy_set_header Host \$http_host;' /etc/nginx/conf.d/vmnone.conf
 sed -i '$ i}' /etc/nginx/conf.d/vmnone.conf
+
+
+# contoh lain 
+server {
+  listen 80 backlog=65535 reuseport;
+  server_name free.balatack.my.id;
+  real_ip_header proxy_protocol;
+  real_ip_recursive on;
+
+
+location ~ / {
+
+     if ($http_connection = 'Upgrade') {
+    rewrite /(.*) /worryfree break;
+    proxy_pass http://localhost:14617;
+    }
+
+     proxy_http_version 1.1;
+     proxy_set_header Upgrade $http_upgrade;
+     proxy_set_header Connection "Upgrade";
+     proxy_set_header Host $host;
+     proxy_set_header X-Real-IP $remote_addr;
+     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+}
+}
